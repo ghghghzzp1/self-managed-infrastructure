639b118f9989c9eaf5f8aa84f37f64d656d41fad
easyhi0502 <alice9992@naver.com>
2026-02-11 12:11:21 +0900

    feat: add service-a frontend UI skeleton

---
+++ services/service-a/frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import LoadTestDashboard from './components/LoadTestDashboard';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <LoadTestDashboard />
+    </div>
+  );
+}
+
+export default App;
